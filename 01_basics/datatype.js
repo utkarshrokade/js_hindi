acbb0373a59960fd7869c8bffa7d1d1b827b588f
utkarshrokade <127274977+utkarshrokade@users.noreply.github.com>
2024-09-26 10:01:49 +0000

    datatypes and variables

---
+++ 01_basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)    //error we are using nodejs not browser0
+
+let name="abc"
+let age=21
+let isloggedIn =true
+
+//number=>2 to power53
+//bigint
+//string
+//boolean
+//null //datatype as well as standalone value 
+//undefined => not defined just declaration not assign
+//symbol=>unique
+
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const id =1345;
+let stname ="utkarsh"
+var prn="2103077" //not more used
+age =15
+let dob; //undefined
+// id =453    error cant change the const variable
+stname ="abc"
+prn="4523"
+age = 20
+/*prefer not use var
+because of scope issue */
+
+
+
+console.log(id);
+console.table([stname,prn,age,dob]) //to print in table format
