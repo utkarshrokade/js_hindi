e3fb283b77460c6889c156f3f83728836f73e6d5
utkarshrokade <127274977+utkarshrokade@users.noreply.github.com>
2024-09-26 10:39:57 +0000

    operator and typeconversion

---
+++ 01_basics/conversionOperator.js
@@ -0,0 +1,23 @@
+let score="33" //ip-33abc op-NaN
+console.log(typeof score);
+let value=Number(score)
+console.log(typeof value);//type convrsion
+console.log(value);
+
+//"33"-33
+//"33abc"-NaN
+//null - 0
+//true -1
+let loggedIn =1
+let booleconert=Boolean(loggedIn)
+console.log(booleconert);
+
+//1-true
+//0-false
+//""-false
+//"abc"-true
+//************************************* operation********************************************* */
+
+let value1 =3
+let negvale = -value1
+console.log(negvale);
